237b4806c8b68eac932e9c7959dcf9d77fe311aa
Colin Fry <colin.fry@tribalworldwide.co.uk>
2022-03-26 12:28:06 +0000

    Initial Commit

---
+++ uMotif-clean/uMotif_cleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  uMotif_cleanApp.swift
+//  uMotif-clean
+//
+//  Created by Colin Fry on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct uMotif_cleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
