5d1d748a195695e9034ea1780cfee1d1c9a6e03e
Colin Fry <colin.fry@tribalworldwide.co.uk>
2022-03-28 10:37:34 +0100

    Used Jokes.example in mock

--- uMotif-cleanTests/Mocks/CNJokesDataSource_TwoJokesMock.swift
+++ uMotif-cleanTests/Mocks/CNJokesDataSource_TwoJokesMock.swift
@@ -13,8 +13,6 @@ struct CNJokesDataSource_TwoJokesMock: CNJokeDataSourceProtocol {
     var dataService: CNJokeServiceProtocol?
     
     func getCNJokes() async throws -> [CNJoke] {
-        let joke1 = CNJoke(id: 0, joke: "Joke1", categories: [])
-        let joke2 = CNJoke(id: 1, joke: "Joke2", categories: [])
-        return [joke1, joke2]
+        return [CNJoke.example, CNJoke.example]
     }
 }
